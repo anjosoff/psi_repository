ee9c13116a826dd800aac5b563719feecef03b02
Guilherme <anjosgui12@gmail.com>
2022-05-30 10:31:55 -0300

    Criação do MVC para Fabricantes

---
+++ psi_project/Controllers/FabricanteController.cs
@@ -0,0 +1,19 @@
+﻿using psi_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace psi_project.Controllers
+{
+    public class FabricanteController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Fabricante
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(c=>c.Nome));
+        }
+    }
+}
