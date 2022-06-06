856dca2db489164233c5b593e83405c26d8eacc3
Guilherme <anjosgui12@gmail.com>
2022-06-06 10:07:28 -0300

    inserindo metodo create e edit no controller de fabricantes

--- psi_project/Controllers/FabricanteController.cs
+++ psi_project/Controllers/FabricanteController.cs
@@ -1,7 +1,9 @@
 ﻿using psi_project.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,11 +11,74 @@ namespace psi_project.Controllers
 {
     public class FabricanteController : Controller
     {
+        private static IList<Fabricante> fabricantes = new List<Fabricante>(){
+            new Fabricante() { FabricanteId = 1, Nome = "fabricantes Carros"},
+            new Fabricante() { FabricanteId = 2, Nome = "fabricantes Motos"},
+            new Fabricante() { FabricanteId = 3, Nome = "fabricantes Impressoras"},
+            new Fabricante() { FabricanteId = 4, Nome = "fabricantes Eletro"},
+            new Fabricante() { FabricanteId = 5, Nome = "fabricantes Avião"},
+            new Fabricante() { FabricanteId = 6, Nome = "fabricantes Notebooks"},
+        };
         private EFContext context = new EFContext();
+
+        public ActionResult Index() => View(fabricantes);
+
+        public ActionResult Create() => View();
         // GET: Fabricante
-        public ActionResult Index()
+        /*public ActionResult Index()
         {
             return View(context.Fabricantes.OrderBy(c=>c.Nome));
+        }/*
+
+
+        // GET: Create
+        /*public ActionResult Create()
+        {
+            return View();
+        }*/
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+            context.Fabricantes.Add(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+            // GET: Fabricantes/Edit/5
+            [HttpGet]
+            public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = fabricantes.Where(m => m.FabricanteId == id).First();
+            //Fabricante fabricante = context.Fabricantes.Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        // POST: Fabricantes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Fabricante fabricante)
+        {
+            if (ModelState.IsValid)
+            {
+                fabricantes.Remove(
+                 fabricantes.Where(c => c.FabricanteId == fabricante.FabricanteId).First());
+                fabricantes.Add(fabricante);
+                //context.Entry(fabricante).State = EntityState.Modified;
+                // context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fabricante);
         }
     }
 }
